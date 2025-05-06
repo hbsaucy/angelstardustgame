c864cc6833709c10152e7e76fdddc439ffd5d165
hbsaucy <henryeliott919@icloud.com>
2025-05-06 16:27:44 +0100

    Add files via upload

---
+++ angelstardustgame.lua
@@ -0,0 +1,81 @@
+
+run as py.jpeg(flash).runlauncher
+
+//spawn stars random location
+Run xpec add plus star run as 0
+
+//lets stars fly through the platform
+
+plus.enter send as location x,y().algorithm/func.run as leave.ui
+
+Add 0 as run .0 + run
+
+//lets star change size 
+
+Run as 0 to 0 + 17 mm d x (shape:star.5mm)
+Run star as 0 + 1 as big and small as 0.5 and 0.5..line run 2 seconds as integer.run py.anode.leave as float.hash node. size 
+
+//lets star change colour
+
+	Function(colour)
+		code colour: black, blue, white, green, purple, yellow, red, orange as integer
+
+	end function() 
+
+//runs as a points system
+
+	length+=aim catch smallest star possible = more points points number from 0-100
+
+	as integer +101
+java/.pyrun 200mxby200mx at 0+0.5 to -+15
+border = line + 2 px 
+
+//runs a randomised funcion for points given to player when catches star
+
+	func()
+
+	random(0,1,2,3,4,5,6,7,8,9as 100+0-1)
+
+//movement for star
+
+run decimal 0 +1 around 0 axioms 15d as line of 0 + 1 
+float with movement.py(poo)
+	end()
+
+//add 3d motion movement for star
+
+axiom.line(int).cap.run_!:@? show loine as zero flows .py(length)
+
+//basecreen background 
+
+500x750px in diamter as background.noise(colour.flash(8ms/.2fade.greyline))
+
+//character movement
+
+
+code line as function add character base to 0.1 
+
+leave line./ as integer 1 and corellate using .macos to ubuntu as pi. 
+
+pi leave line as .com.ul i oso 
+
+iso leave idea as angel with wings 
+
+++upload character design as 0 
+
+//character powwer to catch stars
+
+add + (function) and send to node.js 1(poo)
+
+plus add functoin(poo)
+star as ping.noise + pointscore + random(starvariable)
+
+
+// angel changes colour everytime catches stars 
+
+rerun loop end 
+
+send as .io ux information to graphic render 
+
+add wasd move angel +()_+ add mousetap as enter 
+ 
